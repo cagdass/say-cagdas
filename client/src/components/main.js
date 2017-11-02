7ea9c83bcc3b874731e6913ddbe45b57a6b963f9
cagdas <cagdas@bomonti.mobilearts.local>
2017-11-02 19:39:48 +0100

    Stop committing things before actually compiling you idiot

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,8 +1,25 @@
 import React, { Component, PropTypes } from 'react';
+import { AppBar, MenuItem, IconMenu, IconButton, TextField, RaisedButton, DropDownMenu } from 'material-ui';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import Dialog from 'material-ui/Dialog';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import Slider from 'material-ui/Slider';
 
-class Main extens Component {
+class Main extends Component {
   constructor (props, context) {
     super(props, context);
+  };
+
+  render () {
+    return (
+      <div>Hello world</div>
+    );
   };
 }
 
--- client/webpack-dev-server.config.js
+++ client/webpack-dev-server.config.js
@@ -50,6 +50,16 @@ const config = {
         include: [imagesPath]
       }
     ],
+    rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
   },
 };
 
