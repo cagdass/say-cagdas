2f80fe72d468f854214e15d7b7d4f703d5aa6621
cagdas <cagdas.oztekin@mobilearts.com>
2017-11-07 21:05:57 +0100

    CPU

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ express = require('express');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+var cluster = require('cluster');
+
+var numCPUs = require('os').cpus().length;
 
 const config = require("./config.js");
 
-const app = express();
+
 
 let getIndex = function(len) {
     let first_index = Math.floor(Math.random() * 3);
@@ -20,34 +23,48 @@ let getIndex = function(len) {
 let base_dir = config.tracks_dir;
 let filenames = config.tracks;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
-app.get('/', function (request, response) {
-    let ip = request.headers['x-forwarded-for'] ||
-      request.connection.remoteAddress ||
-      request.socket.remoteAddress ||
-      request.connection.socket.remoteAddress;
-    let fpath = base_dir + filenames[getIndex(filenames.length)];
-    let filestream = fs.createReadStream(fpath);
-    var d = new Date();
-    d.setTime(d.getTime() + d.getTimezoneOffset()*60*1000 );
-    console.log(`Serving file: ${fpath}. Request from ${ip} Time ${d}`);
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-    filestream.on('open', function() {
-        let stats = fs.statSync(fpath);
-        let fileSizeInBytes = stats["size"];
-        response.writeHead(200, {
-            "Accept-Ranges": "bytes",
-            'Content-Type': 'audio/mpeg',
-            'Content-Length': fileSizeInBytes});
-        filestream.pipe(response);
-    });
-})
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  const app = express();
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-app.listen(3009, function () {
-  console.log('Audio file provider listening on port 3009');
-})
+  app.get('/', function (request, response) {
+      let ip = request.headers['x-forwarded-for'] ||
+        request.connection.remoteAddress ||
+        request.socket.remoteAddress ||
+        request.connection.socket.remoteAddress;
+      let fpath = base_dir + filenames[getIndex(filenames.length)];
+      let filestream = fs.createReadStream(fpath);
+      var d = new Date();
+      d.setTime(d.getTime() + d.getTimezoneOffset()*60*1000 );
+      console.log(`Serving file: ${fpath}. Request from ${ip} Time ${d}`);
+
+      filestream.on('open', function() {ls
+
+          let stats = fs.statSync(fpath);
+          let fileSizeInBytes = stats["size"];
+          response.writeHead(200, {
+              "Accept-Ranges": "bytes",
+              'Content-Type': 'audio/mpeg',
+              'Content-Length': fileSizeInBytes});
+          filestream.pipe(response);
+      });
+  })
+
+  app.listen(3009, function () {
+    console.log('Audio file provider listening on port 3009');
+  })
+}
