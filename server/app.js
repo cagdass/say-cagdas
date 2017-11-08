db45f3a68f143882d4323ada351decd9e0c4796a
cagdas <cagdas.oztekin@mobilearts.com>
2017-11-08 20:31:37 +0100

    Finalize server

--- server/app.js
+++ server/app.js
@@ -2,15 +2,13 @@ express = require('express');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
 var bodyParser = require('body-parser');
 
 const config = require("./config.js");
 const base_dir = config.tracks_dir;
 const port = config.port;
 
-let uids = [];
+let ips = [];
 
 let getIndex = function(len) {
   let first_index = Math.floor(Math.random() * 5);
@@ -22,65 +20,55 @@ let getIndex = function(len) {
   }
 }
 
-if (cluster.isMaster) {
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-}
-else {
-  const app = express();
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(bodyParser());
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.post('/', function (request, response) {
+  let { uid } = request.body;
+  if (uid != undefined) {
+    let ind = ips.indexOf(uid);
+    if (ind == -1) {
+      ips.push(uid);
+      let ip = request.headers['x-forwarded-for'] ||
+        request.connection.remoteAddress ||
+        request.socket.remoteAddress ||
+        request.connection.socket.remoteAddress;
+      fs.readdir(base_dir, (err, filenames) => {
+        let fpath = base_dir + filenames[getIndex(filenames.length)];
+        let filestream = fs.createReadStream(fpath);
+        var d = new Date();
+        d.setTime(d.getTime() + d.getTimezoneOffset()*60*1000 );
+        console.log(`Serving file: ${fpath}. Request from ${ip} Time ${d}`);
+        filestream.on('open', function() {
+          fs.stat(fpath, (err, stats) => {
+            let fileSizeInBytes = stats["size"];
+            response.writeHead(200, {
+              "Accept-Ranges": "bytes",
+              'Content-Type': 'audio/mpeg',
+              'Content-Length': fileSizeInBytes});
+            filestream.pipe(response);
+            setTimeout(function(){
+              ind = ips.indexOf(uid);
+              ips.splice(ind, 1);
+            }, 2000)
 
-  app.post('/', function (request, response) {
-    let { uid } = request.body;
-    console.log(`uids: ${uids}, uid: ${uid}`);
-    if (uid != undefined) {
-      let ind = uids.indexOf(uid);
-      console.log(ind);
-      if (ind == -1) {
-        uids.push(uid);
-        let ip = request.headers['x-forwarded-for'] ||
-          request.connection.remoteAddress ||
-          request.socket.remoteAddress ||
-          request.connection.socket.remoteAddress;
-        fs.readdir(base_dir, (err, filenames) => {
-          let fpath = base_dir + filenames[getIndex(filenames.length)];
-          let filestream = fs.createReadStream(fpath);
-          var d = new Date();
-          d.setTime(d.getTime() + d.getTimezoneOffset()*60*1000 );
-          console.log(`Serving file: ${fpath}. Request from ${ip} Time ${d}`);
-          filestream.on('open', function() {
-            fs.stat(fpath, (err, stats) => {
-              let fileSizeInBytes = stats["size"];
-              response.writeHead(200, {
-                "Accept-Ranges": "bytes",
-                'Content-Type': 'audio/mpeg',
-                'Content-Length': fileSizeInBytes});
-              filestream.pipe(response);
-              ind = uids.indexOf(uid);
-              uids.splice(ind, 1);
-            });
           });
-        })
-      }
-      else {
-        response.send(418, "Sorry but I am a teapot");
-      }
+        });
+      })
     }
-  })
+    else {
+      response.status(418).send("Sorry but I am a teapot");
+    }
+  }
+})
 
-  app.listen(port, function () {
-    console.log(`Audio file provider listening on port ${port}`);
-  })
-}
+app.listen(port, function () {
+  console.log(`Audio file provider listening on port ${port}`);
+})
